b545d710b83f638745f93d7dd9601ea3d193758a
KonstantinMaznev <knmaznev@icloud.com>
2023-03-27 21:31:19 +0300

    working on a class Board

--- Sea_battle.py
+++ Sea_battle.py
@@ -10,15 +10,20 @@ class Board:
         for i in range(r*c):
             self.cells.append(i)
 
-    def render(self):
-        rc = self.getRowsCount()
-        cl = self.getColumnsCount()
-
-        for r in range(rc):
-            row = []
-            for b in range(cl):
-                row.append(b)
-            print("|".join(row))
+    # def render(self):
+    #     rc = self.getRowsCount()
+    #     cl = self.getColumnsCount()
+    #
+    #     # for r in range(rc):
+    #     #     row = []
+    #     #     for b in range(cl):
+    #     #         row.append(b)
+    #     #     print("|".join(row))
+    #     field = [[' '] * 8 for n in range(7)]
+    #     print("  0 | 1 | 2 | 3 | 4 | 5 | 6 |")
+    #     for i in range(6):
+    #         row_info = " | ".join(field[i])
+    #         print(f"{i} {row_info}")
 
     def getRowsCount(self):
         return self.rowsCount
@@ -28,12 +33,14 @@ class Board:
 
     def getCells(self):
         return self.cells
+
+
 class Ship:
     def __init__(self,x,y):
         self.x=x
         self.y=y
-
-
+w=Board()
+print(w.__init__())
 # if __name__ == '__main__':
     # get users
     # swich turns
