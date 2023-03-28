2a759e2e9cef6133b718b6c7482d4094bfe4e350
KonstantinMaznev <knmaznev@icloud.com>
2023-03-28 21:41:08 +0300

    add class Player and add hot fix

--- Sea_battle.py
+++ Sea_battle.py
@@ -2,7 +2,12 @@ from random import randint
 class Greet:
     def greeting(self):
         print("Приветствуем вас в игре морской бой")
-class Board:
+
+class Ship:
+    def __init__(self,x,y):
+        self.x=x
+        self.y=y
+class Board(Ship):
     def __init__(self, r = 6, c = 6):
         self.cells = []
         self.rowsCount = r
@@ -39,6 +44,15 @@ class Ship:
     def __init__(self,x,y):
         self.x=x
         self.y=y
+class Player(Ship):
+    def __init__(self):
+        super.__init__()
+
+class Win:
+    if Player()==0:
+        print("Победил Компьютер!")
+    else:
+        print("Победил Игрок!")
 w=Board()
 print(w.__init__())
 # if __name__ == '__main__':
